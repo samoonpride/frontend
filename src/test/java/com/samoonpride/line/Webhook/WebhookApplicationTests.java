17dc6bf9cf0ddac12f94ebba889376aa548dd18b
Natdanai <natdadai.t@ku.th>
2024-01-09 17:29:18 +0700

    Init project

---
+++ src/test/java/com/samoonpride/line/Webhook/WebhookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samoonpride.line.Webhook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebhookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
